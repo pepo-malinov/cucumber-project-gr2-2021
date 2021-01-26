890dcc4aee29ed939de35b58213a185381327233
pepo <pepo@192.168.1.148>
2021-01-26 09:33:40 +0200

    Add implementation for login feature

--- cucmber-project-gr2/src/test/java/uni/pl/fmi/LoginSteps.java
+++ cucmber-project-gr2/src/test/java/uni/pl/fmi/LoginSteps.java
@@ -1,37 +1,38 @@
 package uni.pl.fmi;
 
-import cucumber.api.PendingException;
+import static org.junit.Assert.assertEquals;
+
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import uni.pl.fmi.models.LoginPageModel;
 
 public class LoginSteps {
+	
+	LoginPageModel loginPageModel = new LoginPageModel();
+	
 	@Given("^Портебителя отваря екрана за вход в системата$")
 	public void openLoginScreen() throws Throwable {
-
+		loginPageModel.navigatetoLoginScreen();
 	}
 
 	@When("^Въвежда потребителско име \"([^\"]*)\"$")
-	public void addUserName(String arg1) throws Throwable {
-		// Write code here that turns the phrase above into concrete actions
-		throw new PendingException();
+	public void addUserName(String username) throws Throwable {
+		loginPageModel.setUsername(username);
 	}
 
 	@When("^Въвежда парола \"([^\"]*)\"$")
-	public void addPassword(String arg1) throws Throwable {
-		// Write code here that turns the phrase above into concrete actions
-		throw new PendingException();
+	public void addPassword(String password) throws Throwable {
+		loginPageModel.setPassword(password);
 	}
 
 	@When("^Натиска бутона за вход в системата$")
 	public void clickLoginButton() throws Throwable {
-		// Write code here that turns the phrase above into concrete actions
-		throw new PendingException();
+		loginPageModel.clickLoginButton();
 	}
 
-	@Then("^Вижда съобщение за успех\\.$")
-	public void checkLoginMessage() throws Throwable {
-		// Write code here that turns the phrase above into concrete actions
-		throw new PendingException();
+	@Then("^Вижда съобщение \"([^\"]*)\"\\.$")
+	public void checkLoginMessage(String expectedMessage) throws Throwable {
+		assertEquals(expectedMessage, loginPageModel.getLoginMessage());
 	}
 }
